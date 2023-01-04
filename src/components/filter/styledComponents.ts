e347b9db03abcff801c16e32096d61db1c31c55e
kelly galliani <kelly_92b@hotmail.com>
2023-01-04 18:58:14 -0300

    feature: filterComponent

---
+++ src/components/filter/styledComponents.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledInputSearchBox = styled.fieldset`
+    background-color: aqua;
+    width: 95%;
+    max-width: 574px;
+    margin: 0 auto;
+    border: none;
+    position: relative;
+    input{
+        width: 100%;
+        height: 40px;
+        border-radius: 15px;
+        /* padding: 8px; */
+    }
+    button{
+        position: absolute;
+        top: 32%;
+        right: 16px;
+        background: none;
+        border: none;
+    }
+    
+    
+`
