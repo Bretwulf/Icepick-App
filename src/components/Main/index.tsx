a42a8a9544e989b8331751b27232b8269a83d33d
Milena <milenarodarte@users.noreply.github.com>
2023-01-05 11:00:16 -0300

    main styled component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,6 @@
+import MainStyled from "../../styles/mainStyled";
+import { GlobalStyle } from "../../styles/globalStyles";
+const Main = () => {
+  return <MainStyled></MainStyled>;
+};
+export default Main;
