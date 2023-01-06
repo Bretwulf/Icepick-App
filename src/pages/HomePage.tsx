56a1c1a319ec3c3667d73f99dbc64c64ac4e62f6
Milena <milenarodarte@users.noreply.github.com>
2023-01-06 10:37:00 -0300

    home page added main

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/Header/Header";
+import Main from "../components/main";
 
-export function HomePage () {
-    return (
-        <>
-            <Header/>
-        </>
-    )
+export function HomePage() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
