8b50440df2e078d63c4988b54abc9d986cb73232
Milena <milenarodarte@users.noreply.github.com>
2023-01-05 21:56:23 -0300

    adding router dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,25 @@ import "./App.css";
 import LoginPage from "./components/login/login";
 import RegisterPage from "./components/register/Register";
 import EditPage from "./components/edit/edit";
-import DeletePage from './components/delete/delete';
+import DeletePage from "./components/delete/delete";
 import GetPage from "./components/get/getData";
 import { useModal } from "./contexts/modalContext/modalContext";
 import Modal from "./components/modal/modal";
 import { GlobalStyle } from "./styles/globalStyles";
 import FilterFrases from "./components/filter/filter";
-
-
-
+import { RoutesMain as Routes } from "./Routes";
+import ModalProvider from "./contexts/modalContext/modalContext";
+import UserProvider from "./contexts/userContext";
 function App() {
-
-  const {stateModal, showModal} = useModal()
+  const { stateModal, showModal } = useModal();
 
   return (
     <div className="App">
-      <GlobalStyle/>
-      {/* <LoginPage />
+      <UserProvider>
+        <ModalProvider>
+          <GlobalStyle />
+          <Routes />
+          {/* <LoginPage />
       <div></div>
       <RegisterPage/>
       <div>
@@ -27,11 +29,19 @@ function App() {
       </div>
       <DeletePage/>
       <GetPage/> */}
-      {stateModal && <Modal/>}
-      <button onClick={() => showModal(<LoginPage/>)}>Abrir Modal Login</button>
-      <button onClick={() => showModal(<RegisterPage/>)}>Abrir Modal Cadastro</button>
-      <button onClick={() => showModal(<EditPage/>)}>Abrir Modal de Editar</button>
-      <FilterFrases/>
+          {stateModal && <Modal />}
+          <button onClick={() => showModal(<LoginPage />)}>
+            Abrir Modal Login
+          </button>
+          <button onClick={() => showModal(<RegisterPage />)}>
+            Abrir Modal Cadastro
+          </button>
+          <button onClick={() => showModal(<EditPage />)}>
+            Abrir Modal de Editar
+          </button>
+          <FilterFrases />
+        </ModalProvider>
+      </UserProvider>
     </div>
   );
 }
